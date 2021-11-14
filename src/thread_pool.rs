fbdacf79ea40b2bd473a768f71b23bf71b3653a2
Axel <aperezm@fi.uba.ar>
2021-11-13 22:47:06 -0300

    testing sockets

---
+++ src/thread_pool.rs
@@ -0,0 +1,61 @@
+use crate::message::Message;
+use crate::worker::Worker;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    pub sender: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let (sender, reciver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(reciver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender
+            .send(Message::NewJob(job))
+            .expect("Error in sending Job to Worker"); //TODO chequear si no habria que manejar el error o burbujearlo
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        //println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender
+                .send(Message::Terminate)
+                .expect("Error in sending Job to Worker"); //TODO chequear si no habria que manejar el error o burbujearlo
+        }
+
+        //println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            //println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread
+                    .join()
+                    .expect("there was an error joining the workings");
+            }
+        }
+    }
+}
